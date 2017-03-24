1ef1c1f4d95b6ab54ae001b4194e4a5b151ba735
Andy Grove <andygrove73@gmail.com>
2017-03-24 17:11:31 -0600

    add consts for command values

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,13 @@ use self::spidev::{Spidev, SpidevOptions, SpidevTransfer, SPI_MODE_0};
 
 use std::io::Error;
 
+const CMD_GET_PROTOCOL_VERSION : u8 = 0x01;
+const CMD_SET_SENSOR_COUNT     : u8 = 0x02;
+const CMD_GET_SENSOR_COUNT     : u8 = 0x03;
+const CMD_GET_SENSOR_READING   : u8 = 0x04;
+const CMD_SET_INTERVAL         : u8 = 0x05;
+const CMD_TOGGLE_LED           : u8 = 0x06;
+
 pub struct Octasonic {
     spi: Spidev,
     values: Vec<Vec<u8>>
@@ -54,19 +61,19 @@ impl Octasonic {
     }
 
     pub fn get_sensor_count(&self) -> u8 {
-        let _ = self.transfer(0x20);
+        let _ = self.transfer(CMD_GET_SENSOR_COUNT << 4);
         self.transfer(0x00)
     }
 
     /// get the raw sensor reading
     fn _get_sensor_reading(&self, n: u8) -> u8 {
-        let _ = self.transfer(0x30 | n);
+        let _ = self.transfer(CMD_GET_SENSOR_READING << 4 | n);
         self.transfer(0x00)
     }
 
     pub fn set_sensor_count(&self, n: u8) {
         assert!(n > 0 && n < 9);
-        let _ = self.transfer(0x10 | n);
+        let _ = self.transfer(CMD_SET_SENSOR_COUNT << 4 | n);
     }
 
     fn transfer(&self, b: u8) -> u8 {
