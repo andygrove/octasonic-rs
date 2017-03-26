16989fead4af25ce97f0ec0b90410510ce272019
Andy Grove <andygrove73@gmail.com>
2017-03-26 15:31:44 -0600

    code cleanup and add functions for max distance, interval, and protcol version

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,8 @@ const CMD_GET_SENSOR_COUNT     : u8 = 0x03;
 const CMD_GET_SENSOR_READING   : u8 = 0x04;
 const CMD_SET_INTERVAL         : u8 = 0x05;
 const CMD_TOGGLE_LED           : u8 = 0x06;
+const CMD_SET_MAX_DISTANCE     : u8 = 0x07;
+const CMD_GET_MAX_DISTANCE     : u8 = 0x08;
 
 pub struct Octasonic {
     spi: Spidev,
@@ -34,23 +36,47 @@ impl Octasonic {
         Ok(o)
     }
 
+    pub fn get_protocol_version(&self) -> u8 {
+        self.send_receive(CMD_GET_PROTOCOL_VERSION, 0)
+    }
+
     pub fn get_sensor_count(&self) -> u8 {
-        let _ = self.transfer(CMD_GET_SENSOR_COUNT << 4);
-        self.transfer(0x00)
+        self.send_receive(CMD_GET_SENSOR_COUNT, 0)
     }
 
     pub fn get_sensor_reading(&self, n: u8) -> u8 {
-        let _ = self.transfer(CMD_GET_SENSOR_READING << 4 | n);
-        self.transfer(0x00)
+        self.send_receive(CMD_GET_SENSOR_READING, n)
     }
 
     pub fn set_sensor_count(&self, n: u8) {
-        assert!(n > 0 && n < 9);
-        let _ = self.transfer(CMD_SET_SENSOR_COUNT << 4 | n);
+        self.send(CMD_SET_SENSOR_COUNT, n)
+    }
+
+    pub fn set_interval(&self, n: u8) {
+        self.send(CMD_SET_INTERVAL, n)
+    }
+
+    pub fn set_max_distance(&self, n: u8) {
+        self.send(CMD_SET_MAX_DISTANCE, n )
+    }
+
+    pub fn get_max_distance(&self) -> u8 {
+        self.send_receive(CMD_GET_MAX_DISTANCE, 0)
     }
 
     pub fn toggle_led(&self) {
-        let _ = self.transfer(CMD_TOGGLE_LED << 4);
+        self.send(CMD_TOGGLE_LED, 0)
+    }
+
+    /// send a command and read the result byte
+    fn send_receive(&self, cmd: u8, param: u8) -> u8 {
+        let _ = self.transfer(cmd << 4 | param);
+        self.transfer(0x00)
+    }
+
+    /// send a single byte containing a command and a parameter
+    fn send(&self, cmd: u8, param: u8) {
+        let _ = self.transfer(cmd << 4 | param);
     }
 
     fn transfer(&self, b: u8) -> u8 {
