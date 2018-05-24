a4e440fa5866e7c06405bee388162bb0bd066979
andygrove <andygrove73@gmail.com>
2018-05-24 01:08:01 +0000

    fix example

--- examples/blink.rs
+++ examples/blink.rs
@@ -5,7 +5,7 @@ use std::thread::sleep;
 use std::time::Duration;
 
 fn main() {
-  let octasonic = Octasonic::new(8).unwrap();
+  let mut octasonic = Octasonic::new(8).unwrap();
   loop {
     octasonic.toggle_led();
     sleep(Duration::from_millis(500));
