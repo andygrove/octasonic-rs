8944f5ed500cf6b377a109bbc091b2665f4dc9c1
Andy Grove <andygrove73@gmail.com>
2023-12-09 13:40:31 -0700

    fix clippy warnings

--- examples/piano.rs
+++ examples/piano.rs
@@ -5,55 +5,54 @@ use std::thread::sleep;
 use std::time::Duration;
 
 struct Key {
-  playing: bool,
-  counter: u8
+    playing: bool,
+    counter: u8,
 }
 
-
 fn main() {
+    let octasonic = Octasonic::new(8).unwrap();
 
-  let octasonic = Octasonic::new(8).unwrap();
+    octasonic.set_max_distance(100);
+    octasonic.set_interval(0);
 
-  octasonic.set_max_distance(100);
-  octasonic.set_interval(0);
+    let note: Vec<u8> = vec![60, 62, 64, 65, 67, 69, 71, 72]; // C4 .. C5
 
-  let note : Vec<u8> = vec![60, 62, 64, 65, 67, 69, 71, 72]; // C4 .. C5
+    // init key state
+    let mut key: Vec<Key> = vec![];
+    for _ in 0..8 {
+        key.push(Key {
+            playing: false,
+            counter: 0,
+        });
+    }
 
-  // init key state
-  let mut key : Vec<Key> = vec![];
-  for _ in 0 .. 8 {
-    key.push(Key { playing: false, counter: 0 });
-  }
+    let max_distance: u8 = 20;
 
-  let max_distance : u8 = 20;
+    let max_note_duration = 50;
 
-  let max_note_duration = 50;
+    let channel = 55; // single channel for now
 
-  let channel = 55; // single channel for now
+    loop {
+        for i in 0..8 {
+            // get sensor reading
+            let distance = octasonic.get_sensor_reading(i as u8);
 
-  loop {
-    for i in 0 .. 8 {
+            if key[i].playing {
+                key[i].counter = key[i].counter + 1;
+                if key[i].counter == max_note_duration {
+                    key[i].playing = false;
+                    key[i].counter = 0;
+                }
+            }
 
-      // get sensor reading
-      let distance = octasonic.get_sensor_reading(i as u8);
-
-      if key[i].playing {
-        key[i].counter = key[i].counter+1;
-        if key[i].counter == max_note_duration {
-          key[i].playing = false;
-          key[i].counter = 0;
+            if !key[i].playing && distance < max_distance {
+                key[i].playing = true;
+                key[i].counter = 0;
+                // velocity is a function of distance .. closer is louder
+                let velocity = 127; //50 + max_distance - distance;
+                println!("noteon {} {} {}", channel, note[i], velocity);
+            }
         }
-      }
-
-      if !key[i].playing && distance < max_distance {
-        key[i].playing = true;
-        key[i].counter = 0;
-        // velocity is a function of distance .. closer is louder
-        let velocity = 127; //50 + max_distance - distance;
-        println!("noteon {} {} {}", channel, note[i], velocity);
-      }
-
-    } 
-//    sleep(Duration::from_millis(50));
-  }
+        //    sleep(Duration::from_millis(50));
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,33 +3,33 @@ use self::spidev::{Spidev, SpidevOptions, SpidevTransfer, SPI_MODE_0};
 
 use std::io::Error;
 
-const CMD_GET_PROTOCOL_VERSION : u8 = 0x01;
-const CMD_SET_SENSOR_COUNT     : u8 = 0x02;
-const CMD_GET_SENSOR_COUNT     : u8 = 0x03;
-const CMD_GET_SENSOR_READING   : u8 = 0x04;
-const CMD_SET_INTERVAL         : u8 = 0x05;
-const CMD_TOGGLE_LED           : u8 = 0x06;
-const CMD_SET_MAX_DISTANCE     : u8 = 0x07;
-const CMD_GET_MAX_DISTANCE     : u8 = 0x08;
+const CMD_GET_PROTOCOL_VERSION: u8 = 0x01;
+const CMD_SET_SENSOR_COUNT: u8 = 0x02;
+const CMD_GET_SENSOR_COUNT: u8 = 0x03;
+const CMD_GET_SENSOR_READING: u8 = 0x04;
+const CMD_SET_INTERVAL: u8 = 0x05;
+const CMD_TOGGLE_LED: u8 = 0x06;
+const CMD_SET_MAX_DISTANCE: u8 = 0x07;
+const CMD_GET_MAX_DISTANCE: u8 = 0x08;
 
 pub struct Octasonic {
     spi: Spidev,
 }
 
 impl Octasonic {
-
     /// Create an Octasonic struct for the specified sensor count
     pub fn new(sensor_count: usize) -> Result<Self, Error> {
-        let mut spi = try!(Spidev::open("/dev/spidev0.0"));
+        let mut spi = Spidev::open("/dev/spidev0.0")?;
         let mut options = SpidevOptions::new();
 
-        options.bits_per_word(8)
+        options
+            .bits_per_word(8)
             .max_speed_hz(20_000)
             .mode(SPI_MODE_0);
 
-        try!(spi.configure(&options));
+        spi.configure(&options)?;
 
-        let o = Octasonic { spi: spi };
+        let o = Octasonic { spi };
 
         o.set_sensor_count(sensor_count as u8);
 
@@ -57,7 +57,7 @@ impl Octasonic {
     }
 
     pub fn set_max_distance(&self, n: u8) {
-        self.send(CMD_SET_MAX_DISTANCE, n )
+        self.send(CMD_SET_MAX_DISTANCE, n)
     }
 
     pub fn get_max_distance(&self) -> u8 {
